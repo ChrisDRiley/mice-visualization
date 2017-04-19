4e3c2c17e4590910aa061b2ebb874b89ea7d5795
jmwandu <jmwandu@indiana.edu>
2017-04-18 21:14:08 -0400

    fixed bug

--- src/micevisualization/Grid.java
+++ src/micevisualization/Grid.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Date;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
